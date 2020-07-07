c6e027e29052988ea5ebd03ff11af8dd861fb5ea
Amos <alphatrl1997@gmail.com>
2020-07-08 00:17:38 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="Header">
+        <h1>SMU Shortcuts</h1>
+      </header>
+    </div>
+  );
+}
