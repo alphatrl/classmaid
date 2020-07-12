c5f3e5397d997fd53bce6910d987792c2588f121
Amos <alphatrl1997@gmail.com>
2020-07-12 14:06:04 +0800

    Add about page

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -10,7 +10,22 @@ export const About: React.FC = () => {
         <h1>About</h1>
       </header>
 
-      <main className="mainView">About</main>
+      <main className="mainView">
+        <p>
+          Providing students a quick one-click bookmarks to commonly used sites
+          in SMU.
+        </p>
+        <p>This site is non-affliated with SMU.</p>
+
+        <div className="contribute">
+          <div className="aboutHeader">Contribute</div>
+          <p>
+            Want to suggest more sites? Add an issue or make a pull request
+            on&nbsp;
+            <a href="">Github</a>
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
