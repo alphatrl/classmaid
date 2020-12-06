12ef7764a580a1dd87f15c6803798554904b5a15
Amos Tan <alphatrl1997@gmail.com>
2020-12-07 00:33:56 +0800

    Revert "Add Nav and Ticker"
    This reverts commit cde1834bea266b2b85c4597c70cbbb23388a30f3.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,3 @@ export { default as Card } from './Card';
 export { default as CardList } from './CardList';
 export { default as Footer } from './Footer';
 export { default as List } from './List';
-export { default as NavBar } from './NavBar';
-export { default as Ticker } from './Ticker';
