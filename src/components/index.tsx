dc1689e1ae17e16ca642241041108ee47076a9f4
Amos Tan <alphatrl1997@gmail.com>
2020-12-09 01:40:19 +0800

    Fetch Today's View
    Fetch from public JSON file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Card } from './Card';
 export { default as List } from './List';
 export { default as NavBar } from './NavBar';
 export { default as Ticker } from './Ticker';
+export { default as TodayView } from './TodayView';
