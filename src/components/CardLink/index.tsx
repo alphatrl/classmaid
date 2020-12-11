327535b3d64188c265286c9adbcfe1258b5bdfa4
Amos <alphatrl1997@gmail.com>
2020-12-11 21:18:46 +0800

    Add small card component

--- src/components/CardLink/index.tsx
+++ src/components/CardLink/index.tsx
@@ -5,16 +5,50 @@ import './styles.scss';
 
 interface CardProps {
   cardStyle?: Record<string, unknown>;
+  title?: string;
+  logo?: string;
+  link?: string;
+  newTab?: boolean;
 }
 
-const CardLink: React.FC<CardProps> = ({ cardStyle }: CardProps) => {
-  
-  
+const CardLink: React.FC<CardProps> = ({
+  cardStyle,
+  title,
+  logo,
+  link,
+  newTab,
+}: CardProps) => {
+  const isNewTab = newTab ? '_blank' : '_self';
+
   return (
-    <Card>
-      <></>
+    <Card
+      style={{
+        ...cardStyle,
+        ...{
+          backgroundColor: '#0A0A0A',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }}
+    >
+      <a href={link} className="card-link" target={isNewTab}>
+        <div className="card-container">
+          <div className="image">
+            <span className="material-icons-sharp md-48">{logo}</span>
+          </div>
+          <div className="title">{title}</div>
+        </div>
+      </a>
     </Card>
   );
+};
+
+CardLink.defaultProps = {
+  title: '',
+  logo: 'help_outline',
+  link: 'smu.edu.sg',
+  newTab: true,
 };
 
 export default CardLink;
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -24,6 +24,93 @@ export const Home: React.FC = () => {
 
       <div className="container">
         <TodayView cardStyle={{ gridArea: 'b1' }} />
+
+        <CardLink
+          cardStyle={{ gridArea: 's1' }}
+          title="eLearn"
+          logo="menu_book"
+          link="https://elearn.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's2' }}
+          title="Oasis"
+          logo="beach_access"
+          link="https://elearn.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's3' }}
+          title="Facility Bookings"
+          logo="airline_seat_individual_suite"
+          link="https://elearn.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's4' }}
+          title="Print Services"
+          logo="print"
+          link="https://smu.sg/print"
+        />
+
+        <CardLink
+          cardStyle={{ gridArea: 's5' }}
+          title="Library"
+          logo="book"
+          link="https://library.smu.edu.sg/"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's6' }}
+          title="Student Wellness"
+          logo="favorite"
+          link="https://www.smu.edu.sg/campus-life/student-wellness"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's7' }}
+          title="Guides"
+          logo="forum"
+          link="/guides"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 's8' }}
+          title="Community"
+          logo="groups"
+          link="/community"
+        />
+
+        <CardLink
+          cardStyle={{ gridArea: 'c1' }}
+          title="Boss"
+          logo="account_balance_wallet"
+          link="https://boss.intranet.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 'c2' }}
+          title="onTRAC II"
+          logo="work"
+          link="https://ontrac.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 'c3' }}
+          title="SPS"
+          logo="biotech"
+          link="https://sps.intranet.smu.edu.sg"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 'c4' }}
+          title="Upcoming Events"
+          logo="event"
+          link="https://inet.smu.edu.sg/sites/courses/Pages/Online-Critical-Dates.aspx"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 'c5' }}
+          title="Course Offerings"
+          logo="payments"
+          link="https://inet.smu.edu.sg/sites/courses/Pages/Course-Offerings.aspx"
+        />
+        <CardLink
+          cardStyle={{ gridArea: 'c6' }}
+          title="Community"
+          logo="groups"
+          link="/community"
+        />
       </div>
     </>
   );
