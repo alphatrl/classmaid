983881f138912a0ec5221c3029d3a2d697267901
Amos <alphatrl1997@gmail.com>
2020-12-11 21:18:46 +0800

    Add small card component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Card } from './Card';
+export { default as CardLink } from './CardLink';
 export { default as List } from './List';
 export { default as NavBar } from './NavBar';
 export { default as Ticker } from './Ticker';
