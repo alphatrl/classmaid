1e3008f9ec3778caa0434ab7967a7e50a2641b41
Amos <alphatrl1997@gmail.com>
2020-12-13 01:35:17 +0800

    Update Card component to styled-components

--- src/components/CardLink/index.tsx
+++ src/components/CardLink/index.tsx
@@ -1,45 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from '../index';
-
-import './styles.scss';
+import styled from 'styled-components';
 
 interface CardProps {
   cardStyle?: Record<string, unknown>;
+  gridArea: string;
   title?: string;
   logo?: string;
   link?: string;
   newTab?: boolean;
 }
 
-const CardLink: React.FC<CardProps> = ({
-  cardStyle,
-  title,
-  logo,
-  link,
-  newTab,
-}: CardProps) => {
+const Link = styled.a`
+  text-decoration: none;
+  height: 90%;
+  width: 90%;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  color: #ffffff;
+  transition: 5ms;
+
+  &:hover {
+    color: #2b2b2b;
+  }
+`;
+
+const Image = styled.div`
+  height: 55%;
+  display: flex;
+  justify-content: flex-end;
+  text-decoration: none;
+`;
+
+const Icon = styled.span`
+  font-size: 6vw;
+
+  @media screen and (max-width: 720px) {
+    font-size: 20vw;
+  }
+`;
+
+const Title = styled.span`
+  height: 45%;
+  display: flex;
+  flex-direction: column-reverse;
+  font-weight: 600;
+  font-size: 2vw;
+
+  @media screen and (max-width: 720px) {
+    font-size: 7vw;
+  }
+`;
+
+const CardLink: React.FC<CardProps> = (props) => {
+  const { cardStyle, title, logo, link, newTab, gridArea } = props;
   const isNewTab = newTab ? '_blank' : '_self';
 
   return (
-    <Card
-      style={{
-        ...cardStyle,
-        ...{
-          backgroundColor: '#2b2b2b',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      }}
-    >
-      <a href={link} className="card-link" target={isNewTab}>
-        <div className="card-container">
-          <div className="image">
-            <span className="material-icons-sharp md-48">{logo}</span>
-          </div>
-          <div className="title">{title}</div>
-        </div>
-      </a>
+    <Card gridArea={gridArea}>
+      <Link href={link} className="card-link" target={isNewTab}>
+        <Container>
+          <Image>
+            <Icon className="material-icons-sharp md-48">{logo}</Icon>
+          </Image>
+          <Title>{title}</Title>
+        </Container>
+      </Link>
     </Card>
   );
 };
