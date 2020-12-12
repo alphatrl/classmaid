b122bfe27210cfdd0d1c74fe1115327e39170843
Amos <alphatrl1997@gmail.com>
2020-12-12 17:14:37 +0800

    Move NavBar and main App to styled-components
    - remove stylesheet referenced by these components
    - remove old constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.scss';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home, Guides, Community, About } from './screens';
+
+const Wrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding-bottom: 24px;
+
+  @media (max-width: 720px) {
+    width: 90%;
+  }
+
+  @media (max-width: 350px) {
+    width: 95%;
+  }
+`;
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="App">
+      <Wrapper>
         <Switch>
           <Route exact path="/">
             <Home />
@@ -21,7 +37,7 @@ export const App: React.FC = () => {
             <About />
           </Route>
         </Switch>
-      </div>
+      </Wrapper>
     </BrowserRouter>
   );
 };
