3aa659251fb60e517d980833fecdfe238f9c2463
Amos <alphatrl1997@gmail.com>
2020-12-15 15:53:16 +0800

    Update community and guides
    - Use styled-components
    - Loading UI

--- src/screens/community.tsx
+++ src/screens/community.tsx
@@ -1,9 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import './styles.scss';
-import { List } from '../components';
+import styled from 'styled-components';
 import ReactGA from 'react-ga';
 
+import { NavBar, List } from '../components';
+import logo from '../images/logo-nobg.png';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 12px 0;
+
+  .shortcutsLogo {
+    width: 100px;
+    image-rendering: -webkit-optimize-contrast;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 1.5em;
+  }
+`;
+
+const Container = styled.main`
+  width: 100%;
+  padding: 12px;
+  border: 3px solid #2b2b2b;
+  box-sizing: border-box;
+`;
+
+const Loader = styled.div`
+  margin: 0 auto;
+  border: 8px solid #dcdcdc;
+  border-top: 8px solid #e4a925;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 export const Community: React.FC = () => {
+  const [loading, setLoading] = useState(true);
   const [links, setLinks] = useState({
     telegram: [
       {
@@ -33,6 +81,7 @@ export const Community: React.FC = () => {
         }
       );
       setLinks(link);
+      setLoading(false);
     };
 
     document.title = 'SMU Shortcuts | Community';
@@ -41,19 +90,25 @@ export const Community: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <header className="header">
+    <>
+      <Header>
+        <img src={logo} className="shortcutsLogo" alt="smu-shortcut icon"></img>
         <h1>Community</h1>
-      </header>
+      </Header>
+      <NavBar />
 
-      <main className="mainView">
-        <List title="Telegram" data={links.telegram} />
-        <div className="paddingList" />
-        <List title="Instagram" data={links.instagram} />
-        <div className="paddingList" />
-        <List title="Websites" data={links.websites} />
-      </main>
-    </div>
+      <Container>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <List title="Telegram" data={links.telegram} /> <br />
+            <List title="Instagram" data={links.instagram} /> <br />
+            <List title="Websites" data={links.websites} />
+          </>
+        )}
+      </Container>
+    </>
   );
 };
 
