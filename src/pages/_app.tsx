88c77b869d2b3ab2125146076bb05b7c7841e179
Duncan Leo <hello@duncanleo.me>
2021-03-27 10:58:09 +0800

    chore: restore analytics init code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../theme';
+import Analytics from '../components/Analytics';
 
 const App: React.FC<AppProps> = function (props) {
   const { Component, pageProps } = props;
 
   return (
     <ThemeProvider theme={theme}>
+      <Analytics />
       <Component {...pageProps} />
     </ThemeProvider>
   );
