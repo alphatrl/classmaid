1457a4ef1bbd302deb4b6e01c00fb79e19228bbe
Amos Tan <alphatrl1997@gmail.com>
2023-01-02 19:35:53 +0800

    refactor: rename component

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { CalendarList } from '../components/cards';
+import { CalendarList } from '../components/card';
 import DefaultLayout from '../layouts/DefaultLayout';
 
 const Wrapper = styled.div`
