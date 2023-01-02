448fb405256ada7bc93bf559f0e32638e84561b1
Amos Tan <alphatrl1997@gmail.com>
2023-01-02 19:34:10 +0800

    refactor: create new wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,33 @@
-// import { isArray } from 'lodash';
-// import dynamic from 'next/dynamic';
-// import { useRouter } from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
 
-import { AppLibrary, CapacityCard, TodayCard } from '../components/Card/';
 import DefaultLayout from '../layouts/DefaultLayout';
 
-const Wrapper = styled.div`
+const ContentWrapper = styled.div`
   box-sizing: border-box;
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  column-gap: 16px;
-
-  padding-top: 24px;
-  padding-bottom: max(24px, env(safe-area-inset-bottom));
-  padding-left: max(16px, env(safe-area-inset-left));
-  padding-right: max(16px, env(safe-area-inset-right));
-
-  @media screen and (max-width: ${(props) => props.theme.mobileSize}) {
-    display: flex;
-    flex-direction: column;
-    overflow-y: auto;
-
-    padding-top: 16px;
-    padding-bottom: max(16px, env(safe-area-inset-bottom));
-    padding-left: max(8px, env(safe-area-inset-left));
-    padding-right: max(8px, env(safe-area-inset-right));
-  }
+  display: flex;
+  justify-content: center;
 `;
 
-const WidgetContainer = styled.div`
+const Wrapper = styled.div`
+  width: 1035px;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+
+  padding-top: 40px;
+  padding-bottom: 64px;
+
+  @media screen and (max-width: 1000px) {
+    width: 800px;
+  }
 `;
 
 export const Home: React.FC = () => {
   return (
-    <DefaultLayout title="Home">
-      <Wrapper>
-        <WidgetContainer>
-          <TodayCard />
-          <CapacityCard />
-        </WidgetContainer>
-        <AppLibrary />
-      </Wrapper>
+    <DefaultLayout title="SMU">
+      <ContentWrapper>
+        <Wrapper></Wrapper>
+      </ContentWrapper>
     </DefaultLayout>
   );
 };
