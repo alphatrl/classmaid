091b0d59eaf7a050ea6883215eb247865c7241cc
Amos Tan <alphatrl1997@gmail.com>
2023-01-03 21:31:00 +0800

    style(card): Update `CardTemplate`

--- src/components/card/styled.tsx
+++ src/components/card/styled.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
-export const CardTemplate = styled.div`
+export const CardTemplate = styled.div<{ width: number; height?: number }>`
   box-sizing: border-box;
   padding: 16px;
-  border-radius: 16px;
+  border-radius: 24px;
   background: #f1f1f1;
   box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.25);
 
+  width: ${(props) => props.width}px;
+  height: ${(props) => (props.height != null ? props.height : props.width)}px;
+
   @supports (backdrop-filter: ${(props) => props.theme.blur.blur}) {
-    background: ${(props) => props.theme.blur.background};
-    backdrop-filter: ${(props) => props.theme.blur.blur};
+    backdrop-filter: blur(12px);
   }
 `;
