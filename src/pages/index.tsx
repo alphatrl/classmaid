8c0daf12fccd6b1d00749984d5e69ac5e35f5891
Amos Tan <alphatrl1997@gmail.com>
2023-01-03 23:55:41 +0800

    style: add constant theme sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import TodaySummaryWidget from '../components/card/TodaySummaryWidget';
 import DefaultLayout from '../layouts/DefaultLayout';
+import {
+  DESKTOP_WIDTH_SIZE_M,
+  DESKTOP_WIDTH_SIZE_S,
+  MOBILE_WIDTH_SIZE,
+} from '../themes/size';
 
 const ContentWrapper = styled.div`
   box-sizing: border-box;
@@ -10,19 +16,24 @@ const ContentWrapper = styled.div`
 `;
 
 const Wrapper = styled.div`
-  width: 1050px;
+  width: 1200px;
   display: flex;
   flex-wrap: wrap;
 
   padding-top: 40px;
   padding-bottom: 64px;
 
-  @media screen and (max-width: 1100px) {
-    width: 900px;
+  @media screen and (max-width: ${DESKTOP_WIDTH_SIZE_M}) {
+    width: 1000px;
   }
 
-  @media screen and (max-width: 1000px) {
+  @media screen and (max-width: ${DESKTOP_WIDTH_SIZE_S}) {
     width: 800px;
+  }
+
+  @media screen and (max-width: ${MOBILE_WIDTH_SIZE}) {
+    width: 90%;
+    justify-content: center;
   }
 `;
 
@@ -30,7 +41,9 @@ export const Home: React.FC = function () {
   return (
     <DefaultLayout title="SMU">
       <ContentWrapper>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <TodaySummaryWidget />
+        </Wrapper>
       </ContentWrapper>
     </DefaultLayout>
   );
---
+++ src/themes/size.ts
@@ -0,0 +1,5 @@
+export const DESKTOP_WIDTH_SIZE_S = '960px';
+export const DESKTOP_WIDTH_SIZE_M = '1280px';
+export const DESKTOP_WIDTH_SIZE_L = '1440px';
+
+export const MOBILE_WIDTH_SIZE = '820px';
