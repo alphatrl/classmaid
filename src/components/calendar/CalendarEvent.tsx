8c26c3dc386110a1cf692ff724c96abd11eb0626
Amos Tan <alphatrl1997@gmail.com>
2023-01-03 23:54:50 +0800

    feat(today-widget): Add calendar events

---
+++ src/components/calendar/CalendarEvent.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  calendarEvent: App.Calendar.Event;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+
+  :hover {
+    background-color: ${(props) => props.theme.calendar.red}27;
+    border-radius: 0 12px 12px 0;
+  }
+`;
+
+const Divider = styled.div`
+  width: 4px;
+  height: 100%;
+  background-color: ${(props) => props.theme.calendar.red};
+  border-radius: 4px;
+`;
+
+const TextWrapper = styled.div`
+  margin-left: 4px;
+  padding: 4px 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+  font-weight: 600;
+  font-size: 1rem;
+  color: ${(props) => props.theme.text900};
+
+  // NOTE: (hello@amostan.me) This is to truncate the title into 1 line with ellipsis
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  display: -webkit-box;
+  overflow: hidden;
+`;
+
+const Subtitle = styled.p`
+  margin: 0;
+  font-size: 1rem;
+  color: ${(props) => props.theme.text600};
+`;
+
+const CalendarEvent: React.FC<Props> = function (props) {
+  const { calendarEvent } = props;
+
+  return (
+    <Wrapper>
+      <Divider />
+      <TextWrapper>
+        <Title>{calendarEvent.title}</Title>
+        <Subtitle>{calendarEvent.timeString}</Subtitle>
+      </TextWrapper>
+    </Wrapper>
+  );
+};
+
+export default CalendarEvent;
--- src/components/card/TodaySummaryWidget/components/Calendar.tsx
+++ src/components/card/TodaySummaryWidget/components/Calendar.tsx
@@ -1,13 +1,18 @@
-import moment from 'moment-timezone';
 import React from 'react';
 import styled from 'styled-components';
+
+import CalendarEvent from '../../../calendar/CalendarEvent';
 
 const Wrapper = styled.div`
   background-color: #d9d9d9;
   margin-top: 8px;
+
   display: flex;
-  flex-grow: 1;
+  flex: 1;
+  flex-direction: column;
+
   border-radius: 16px;
+  padding: 12px;
 `;
 
 interface Props {
@@ -16,8 +21,18 @@ interface Props {
 
 const Calendar: React.FC<Props> = function (props) {
   const { events } = props;
+  const trimmedEvents = events.length > 3 ? events.slice(0, 3) : events;
 
-  return <Wrapper>ss</Wrapper>;
+  return (
+    <Wrapper>
+      {trimmedEvents.map((calEvent, index) => (
+        <CalendarEvent key={index} calendarEvent={calEvent} />
+      ))}
+      {/* {trimmedEvents.map((calEvent, index) => (
+        <CalendarEvent key={index} calendarEvent={calEvent} />
+      ))} */}
+    </Wrapper>
+  );
 };
 
 export default Calendar;
