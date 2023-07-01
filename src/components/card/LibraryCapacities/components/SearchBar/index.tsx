06cc9f609563fcf3d64ca8af03d5e4109b459f9b
Amos Tan <alphatrl1997@gmail.com>
2023-07-01 12:02:26 +0800

    WIP: Search Bar

--- src/components/card/LibraryCapacities/components/SearchBar/index.tsx
+++ src/components/card/LibraryCapacities/components/SearchBar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+import Icon from '../../../../Icon';
+
+const Wrapper = styled.form`
   box-sizing: border-box;
   width: 100%;
   max-height: 48px;
@@ -11,7 +13,7 @@ const Wrapper = styled.div`
   gap: 12px;
 `;
 
-const InputWrapper = styled.input`
+const SearchInput = styled.input`
   box-sizing: border-box;
   width: 100%;
   height: 100%;
@@ -20,7 +22,7 @@ const InputWrapper = styled.input`
   text-indent: 8px;
 
   border-style: solid;
-  border-radius: 12px;
+  border-radius: 16px;
   border-width: 2px;
   border-color: ${(props) => props.theme.appColor[90]};
 
@@ -29,21 +31,64 @@ const InputWrapper = styled.input`
   }
 `;
 
-const SearchButton = styled.button`
+const SearchButton = styled.button<{ isDisabled: boolean }>`
   box-sizing: border-box;
-  width: 20%;
-  max-width: 92px;
+  width: 64px;
 
   border: none;
+  border-radius: 16px;
   font-size: 1.15rem;
   padding: 12px;
+  color: ${(props) => props.theme.textColor[50]};
+  background-color: ${(props) => props.theme.primary[50]};
+  cursor: pointer;
+
+  transition: background-color 0.3s ease-out;
+
+  :disabled {
+    cursor: default;
+    color: ${(props) => props.theme.textColor[50]};
+    background-color: ${(props) => props.theme.primary[30]};
+  }
 `;
 
 const SearchBar: React.FC = function () {
+  const [searchText, setSearchText] = React.useState('');
+  const isSearchButtonDisabled = searchText.length === 0;
+
+  const handleChangeText = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchText(event.target.value);
+    },
+    []
+  );
+
+  const handleSubmit = React.useCallback(
+    (event: React.FormEvent<HTMLButtonElement>) => {
+      const encodeSearchTerm = encodeURI(searchText);
+      const libraryUrlString = `https://search.library.smu.edu.sg/discovery/search?vid=65SMU_INST:SMU_NUI&tab=Everything&query=any,contains,${encodeSearchTerm}&search_scope=Everything`;
+      window.open(libraryUrlString, '_blank');
+      event.preventDefault();
+    },
+    [searchText]
+  );
+
   return (
     <Wrapper>
-      <InputWrapper placeholder="Search" />
-      <SearchButton>Search</SearchButton>
+      <SearchInput
+        value={searchText}
+        placeholder="Search"
+        onChange={handleChangeText}
+      />
+      <SearchButton
+        type="submit"
+        aria-label="Search SMU Libraries"
+        disabled={isSearchButtonDisabled}
+        isDisabled={isSearchButtonDisabled}
+        onSubmit={handleSubmit}
+      >
+        <Icon name="search" />
+      </SearchButton>
     </Wrapper>
   );
 };
