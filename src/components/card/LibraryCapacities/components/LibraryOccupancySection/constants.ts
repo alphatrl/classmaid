a3d3675af9db795b2fd532983c8c66d0f0b527f2
Amos Tan <alphatrl1997@gmail.com>
2023-07-02 12:43:51 +0800

    refactor: remove emoji

--- src/components/card/LibraryCapacities/components/LibraryOccupancySection/constants.ts
+++ src/components/card/LibraryCapacities/components/LibraryOccupancySection/constants.ts
@@ -1,5 +1,5 @@
 export const LKS_MAX_OCCUPANCY = 1800;
 export const KGC_MAX_OCCUPANCY = 500;
 
-export const LKS_LIBRARY_NAME = '📚 Li Ka Shing Library';
-export const KGC_LIBRARY_NAME = '⚖️ Kwa Geok Choo Law Library';
+export const LKS_LIBRARY_NAME = 'Li Ka Shing Library';
+export const KGC_LIBRARY_NAME = 'Kwa Geok Choo Law Library';
