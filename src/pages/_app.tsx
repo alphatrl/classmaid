cb37524da8d7f8787312d9b422f533e4190c6eb8
Amos Tan <alphatrl1997@gmail.com>
2023-11-26 14:52:18 +0800

    feat(analytics): Add to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 import React from 'react';
 
@@ -13,6 +14,7 @@ const App: React.FC<AppProps> = function (props) {
       <DataWrapper>
         <GlobalStyles />
         <Component {...pageProps} />
+        <Analytics />
       </DataWrapper>
     </ThemeContext>
   );
