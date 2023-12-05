490ee04e8c6ec43d8aeb0a9b5692ca9841102737
Amos Tan <alphatrl1997@gmail.com>
2023-12-05 23:24:49 +0800

    fix(ios): Add `z-index` for modal to take higher precedence

--- src/screens/smu/components/modal/shared/Modal/components/ModalContent.tsx
+++ src/screens/smu/components/modal/shared/Modal/components/ModalContent.tsx
@@ -14,6 +14,9 @@ const ModalOverlay = styled(FloatingOverlay)`
   background: rgba(0, 0, 0, 0.8);
   display: grid;
   place-items: center;
+
+  // NOTE: (hello@amostan.me) This is to counter library image z-index
+  z-index: 5;
 `;
 
 const ModalContentWrapper = styled.div`
