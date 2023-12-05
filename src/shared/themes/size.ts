f444e1f3493f03bda66c29ec2a4ab0aa7425d911
Amos Tan <alphatrl1997@gmail.com>
2023-12-06 00:45:19 +0800

    chore: remove unused desktop size

--- src/shared/themes/size.ts
+++ src/shared/themes/size.ts
@@ -1,18 +1,3 @@
-export const DESKTOP_WIDTH_SIZE_S = '960px';
-export const DESKTOP_WIDTH_SIZE_M = '1280px';
-export const DESKTOP_WIDTH_SIZE_L = '1440px';
-
-export const MOBILE_WIDTH_SIZE_S = '720px';
-export const MOBILE_WIDTH_SIZE_L = '800px';
-
-// for square widget
-export const WIDGET_S_WIDTH_SIZE_S = 300;
-export const WIDGET_S_WIDTH_SIZE_L = 320;
-
-// for rectangle widget
-export const WIDGET_L_WIDTH_SIZE_S = 540;
-export const WIDGET_L_WIDTH_SIZE_L = 640;
-
 export const DESKTOP_WIDTH = 1160;
 export const MOBILE_WIDTH = 760;
 
