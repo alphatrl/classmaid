f7ce9b42052783091ba18c4362d89b3e85fc5d34
Amos Tan <alphatrl1997@gmail.com>
2023-12-05 23:24:12 +0800

    fix(ios): Add padding for header in `display-mode: standalone` (#120)

--- src/shared/components/navigation/NavHeader/index.tsx
+++ src/shared/components/navigation/NavHeader/index.tsx
@@ -5,12 +5,13 @@ import MoreButton from './components/MoreButton';
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 64px;
-  padding-left: max(24px, env(safe-area-inset-top));
-  padding-right: max(12px, env(safe-area-inset-bottom));
+  min-height: 64px;
+  padding-top: max(8px, env(safe-area-inset-top));
+  padding-bottom: 8px;
+  padding-left: max(16px, env(safe-area-inset-left));
+  padding-right: max(16px, env(safe-area-inset-right));
   box-sizing: border-box;
   background: #0000008f;
-
   display: flex;
   align-items: center;
   justify-content: space-between;
