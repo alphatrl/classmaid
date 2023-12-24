316956734383923a4dde60d0133a25c55dfb7abd
Amos Tan <alphatrl1997@gmail.com>
2023-12-24 13:34:04 +0800

    feat(SMU): fetch dates as `getServerSideProps`

--- src/pages/smu/calendar.tsx
+++ src/pages/smu/calendar.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
 import React from 'react';
 import styled from 'styled-components';
 
-import DefaultLayout from '../../shared/components/layouts/DefaultLayout';
 import {
-  MOBILE_MEDIA_QUERY,
-  TABLET_MEDIA_QUERY,
-} from '../../shared/themes/size';
+  IMPORTANT_DATES_URL,
+  SCHOOL_TERM_URL,
+} from '../../screens/smu/constants';
+import DefaultLayout from '../../shared/components/layouts/DefaultLayout';
+import { getCurrentEvent } from '../../shared/contexts/utils';
+import { MOBILE_MEDIA_QUERY } from '../../shared/themes/size';
+
+export interface SMUCalendarServerSideProps {
+  currentEvent: App.Calendar.CurrentEvent | null;
+  schoolTerms: App.Calendar.SchoolTerm[];
+  importantDates: App.Calendar.ImportantDate[];
+}
 
 const ContentWrapper = styled.div`
   box-sizing: border-box;
@@ -15,24 +27,40 @@ const ContentWrapper = styled.div`
 `;
 
 const Wrapper = styled.div`
-  width: 1035px;
   display: flex;
   flex-wrap: wrap;
-  padding-top: 36px;
-  padding-bottom: 64px;
-
-  @media screen and ${TABLET_MEDIA_QUERY} {
-    width: 690px;
-  }
+  background-color: pink;
+  flex: 1;
 
   @media screen and ${MOBILE_MEDIA_QUERY} {
     padding: 16px 0;
-    width: 350px;
     justify-content: center;
   }
 `;
 
-export const Calendar: NextPage = function () {
+export const getServerSideProps = (async () => {
+  const requestPromises = await Promise.all([
+    fetch(SCHOOL_TERM_URL),
+    fetch(IMPORTANT_DATES_URL),
+  ]);
+
+  const schoolTermsJson = await requestPromises[0].json();
+  const importantDatesJson =
+    (await requestPromises[1].json()) as App.Calendar.ImportantDate[];
+
+  const schoolTerms = schoolTermsJson.terms as App.Calendar.SchoolTerm[];
+  const currentEvent = getCurrentEvent(schoolTerms);
+
+  return {
+    props: {
+      currentEvent,
+      schoolTerms,
+      importantDates: importantDatesJson,
+    },
+  };
+}) satisfies GetServerSideProps<SMUCalendarServerSideProps>;
+
+const SMUCalendar: NextPage<SMUCalendarServerSideProps> = function (props) {
   return (
     <DefaultLayout title="SMU">
       <ContentWrapper>
@@ -44,4 +72,4 @@ export const Calendar: NextPage = function () {
   );
 };
 
-export default Calendar;
+export default SMUCalendar;
