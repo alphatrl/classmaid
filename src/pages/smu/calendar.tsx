8918935e2a23e6c4ab1b9fdc10dedf7862a4b1fe
Amos Tan <alphatrl1997@gmail.com>
2023-12-24 13:44:31 +0800

    WIP: move content to `/screens` dir

--- src/pages/smu/calendar.tsx
+++ src/pages/smu/calendar.tsx
@@ -6,9 +6,9 @@ import {
   IMPORTANT_DATES_URL,
   SCHOOL_TERM_URL,
 } from '../../screens/smu/constants';
+import Calendar from '../../screens/smu/pages/Calendar';
 import DefaultLayout from '../../shared/components/layouts/DefaultLayout';
 import { getCurrentEvent } from '../../shared/contexts/utils';
-import { MOBILE_MEDIA_QUERY } from '../../shared/themes/size';
 
 export interface SMUCalendarServerSideProps {
   currentEvent: App.Calendar.CurrentEvent | null;
@@ -20,18 +20,6 @@ const ContentWrapper = styled.div`
   box-sizing: border-box;
   display: flex;
   justify-content: center;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  background-color: pink;
-  flex: 1;
-
-  @media screen and ${MOBILE_MEDIA_QUERY} {
-    padding: 16px 0;
-    justify-content: center;
-  }
 `;
 
 export const getStaticProps = (async () => {
@@ -64,9 +52,7 @@ const SMUCalendar: NextPage<SMUCalendarServerSideProps> = function (props) {
   return (
     <DefaultLayout title="SMU">
       <ContentWrapper>
-        <Wrapper>
-          <h1>WIP</h1>
-        </Wrapper>
+        <Calendar />
       </ContentWrapper>
     </DefaultLayout>
   );
