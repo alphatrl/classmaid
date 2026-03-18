17cf9a0a9be0b7b4c785192a0544ea72774d8f50
Amos Tan <hello@amostan.me>
2026-03-18 15:44:22 +0800

    fix(images): Update SMU libraries images (#154)

--- src/screens/smu/components/widget/LibraryCapacities/components/LibraryOccupancySection/constants.ts
+++ src/screens/smu/components/widget/LibraryCapacities/components/LibraryOccupancySection/constants.ts
@@ -9,7 +9,7 @@ export const LKS_LIBRARY_TEMPLATE: LibraryData = {
     pageUrl:
       'https://commons.wikimedia.org/wiki/File:Singapore_Management_University_-_Li_Ka_Shing_Library.jpg',
     fileUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Singapore_Management_University_-_Li_Ka_Shing_Library.jpg/640px-Singapore_Management_University_-_Li_Ka_Shing_Library.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Singapore_Management_University_-_Li_Ka_Shing_Library.jpg/500px-Singapore_Management_University_-_Li_Ka_Shing_Library.jpg',
     attribution: {
       author: 'Hong Huazheng',
       publishedDate: '10 November 2018',
@@ -31,7 +31,7 @@ export const KGC_LIBRARY_TEMPLATE: LibraryData = {
     pageUrl:
       'https://commons.wikimedia.org/wiki/File:Kwa_Geok_Choo_Law_Library,_School_of_Law,_Singapore_Management_University,_at_night_-_20170107-01.jpg',
     fileUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Kwa_Geok_Choo_Law_Library%2C_School_of_Law%2C_Singapore_Management_University%2C_at_night_-_20170107-01.jpg/640px-Kwa_Geok_Choo_Law_Library%2C_School_of_Law%2C_Singapore_Management_University%2C_at_night_-_20170107-01.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Kwa_Geok_Choo_Law_Library%2C_School_of_Law%2C_Singapore_Management_University%2C_at_night_-_20170107-01.jpg/960px-Kwa_Geok_Choo_Law_Library%2C_School_of_Law%2C_Singapore_Management_University%2C_at_night_-_20170107-01.jpg',
     attribution: {
       author: 'Smuconlaw',
       publishedDate: '7 January 2017',
