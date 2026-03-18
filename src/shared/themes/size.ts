1a15b6398fda6906ea91a922893d987706de4d6a
Amos Tan <alphatrl1997@gmail.com>
2026-03-18 18:17:08 +0800

    refactor: remove unused var

--- src/shared/themes/size.ts
+++ src/shared/themes/size.ts
@@ -2,8 +2,6 @@ export const DESKTOP_WIDTH = 1160;
 export const MOBILE_WIDTH = 760;
 
 export const WIDGET_HEIGHT = 315;
-export const WIDGET_WIDTH_SQUARE = WIDGET_HEIGHT;
-export const WIDGET_WIDTH_RECTANGLE = 640;
 
 export const MOBILE_MEDIA_QUERY = `(max-width: ${MOBILE_WIDTH - 1}px)`;
 export const MOBILE_MEDIA_QUERY_S = `(max-width: 480px)`;
