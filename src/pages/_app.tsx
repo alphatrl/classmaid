8d26cd4224e3124846a605711773ba9e3c8adcd2
Amos Tan <alphatrl1997@gmail.com>
2026-03-20 00:45:57 +0800

    refactor(theme): Remove ThemeContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,15 @@ import { AppProps } from 'next/app';
 import React from 'react';
 
 import { DataWrapper } from '../screens/smu/contexts/DataContext';
-import { ThemeContext } from '../shared/contexts/ThemeContext';
-import { GlobalStyles } from '../theme';
 
 const App: React.FC<AppProps> = function (props) {
   const { Component, pageProps } = props;
 
   return (
-    <ThemeContext>
-      <DataWrapper>
-        <GlobalStyles />
-        <Component {...pageProps} />
-        <Analytics />
-      </DataWrapper>
-    </ThemeContext>
+    <DataWrapper>
+      <Component {...pageProps} />
+      <Analytics />
+    </DataWrapper>
   );
 };
 
