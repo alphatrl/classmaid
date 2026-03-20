dfbfad38f128d851f19d7728aef168cf5c1e482b
Amos Tan <alphatrl1997@gmail.com>
2026-03-20 16:28:18 +0800

    refactor(today-summary): Move to tailwind

--- src/screens/smu/components/widget/TodaySummary/index.tsx
+++ src/screens/smu/components/widget/TodaySummary/index.tsx
@@ -1,22 +1,12 @@
 import moment from 'moment-timezone';
 import Link from 'next/link';
 import React from 'react';
-import styled from 'styled-components';
 
 import { useDataContext } from '../../../contexts/DataContext';
 import { CardTemplate } from '../styled';
 import Calendar from './components/Calendar';
 import TodayEvent from './components/TodayEvent';
 import TodaysSummary from './components/TodaysSummary';
-
-const Card = styled(CardTemplate)`
-  display: flex;
-  flex-direction: column;
-`;
-
-const CardLink = styled(Link)`
-  text-decoration: none;
-`;
 
 const TodaySummaryWidget: React.FC = function () {
   const { currentEvent, calendarEvents } = useDataContext();
@@ -33,7 +23,6 @@ const TodaySummaryWidget: React.FC = function () {
       return [];
     }
 
-    // get currently happening events
     const midnight = moment(today).set({
       hour: 0,
       minute: 0,
@@ -43,21 +32,26 @@ const TodaySummaryWidget: React.FC = function () {
 
     const events = calendarEvents[`${midnight.unix()}`] ?? [];
 
-    // NOTE: (hello@amostan.me) Check for event which are currently ongoing
     return events.filter((calEvent) => {
       const eventEnd = moment.unix(calEvent.endDate);
       return today.isSameOrBefore(eventEnd);
     });
-  }, [calendarEvents, today]);
+  }, [calendarEvents, today, mounted]);
+
+  // Render a placeholder until client mount to avoid hydration mismatch
+  // caused by moment() producing different times on server vs client
+  if (!mounted) {
+    return <CardTemplate className="flex flex-col" />;
+  }
 
   return (
-    <CardLink href="/smu/calendar">
-      <Card>
+    <Link href="/smu/calendar" className="no-underline">
+      <CardTemplate className="flex flex-col">
         <TodayEvent currentEvent={currentEvent} />
         <TodaysSummary todayMoment={today} numOfEvents={todayEvents.length} />
         <Calendar events={todayEvents} />
-      </Card>
-    </CardLink>
+      </CardTemplate>
+    </Link>
   );
 };
 
