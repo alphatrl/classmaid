af85a3d1041d5804d2d58cc9585d15db1e02df7f
Amos Tan <alphatrl1997@gmail.com>
2026-03-20 14:31:47 +0800

    refactor(document): Remove styled-component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,85 +1,52 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
-import React from 'react';
-import { ServerStyleSheet } from 'styled-components';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const { renderPage } = ctx;
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0&display=swap"
+        />
 
-    const initialProps = await Document.getInitialProps(ctx);
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
-    );
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#111111" />
 
-    const styleTags = sheet.getStyleElement();
+        <link rel="manifest" href="/manifest.json" />
 
-    return { ...initialProps, ...page, styleTags };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        {
-          <Head>
-            {/*@ts-ignore*/}
-            {this.props.styleTags}
-
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0&display=swap"
-            />
-
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta
-              name="apple-mobile-web-app-status-bar-style"
-              content="black-translucent"
-            />
-            <meta name="theme-color" content="#111111" />
-
-            <link rel="manifest" href="/manifest.json" />
-
-            <link
-              rel="icon"
-              type="image/png"
-              href="/images/favicon@16x16.png"
-              sizes="16x16"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="/images/favicon@24x24.png"
-              sizes="24x24"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="/images/favicon.png"
-              sizes="32x32"
-            />
-            <link
-              rel="shortcut icon"
-              type="image/svg+xml"
-              href="/images/favicon.svg"
-            />
-          </Head>
-        }
-        <body>
-          <Main />
-          <div id="modal-root"></div>
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/favicon@16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/favicon@24x24.png"
+          sizes="24x24"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/images/favicon.png"
+          sizes="32x32"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/svg+xml"
+          href="/images/favicon.svg"
+        />
+      </Head>
+      <body>
+        <Main />
+        <div id="modal-root"></div>
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
