1c27f5ebda102e1a5c02dc21a15aaf59446fb424
Amos Tan <alphatrl1997@gmail.com>
2026-03-27 23:52:38 +0800

    refactor(dashboard): Move more button to radix

--- src/shared/components/navigation/NavHeader/components/MoreButton.tsx
+++ src/shared/components/navigation/NavHeader/components/MoreButton.tsx
@@ -1,47 +1,24 @@
-import {
-  autoUpdate,
-  safePolygon,
-  useDismiss,
-  useFloating,
-  useFocus,
-  useHover,
-  useInteractions,
-} from '@floating-ui/react';
+import { DropdownMenu } from 'radix-ui';
 import classnames from 'classnames';
 import React from 'react';
 
 import useMediaQuery from '../../../../hooks/useMediaQuery';
 import useMobileDevice from '../../../../hooks/useMobileDevice';
 import Icon from '../../../Icon';
-import MenuItem from '../../../modal/MenuItem';
-import { PopperHeader, PopperWrapper } from '../../../modal/styled';
 import AboutModal from './AboutModal';
 import AddToHomeModal from './AddToHomeModal';
 
+const ItemClassName = classnames(
+  'text-base flex items-center gap-2 px-2 py-1 min-h-12'
+);
+
 const MoreButton: React.FC = function () {
-  const [isOpen, setIsOpen] = React.useState(false);
   const [isAboutModalOpen, setIsAboutModalOpen] = React.useState(false);
   const [isAddToHomeOpen, setIsAddToHomeOpen] = React.useState(false);
 
   const { isMobile } = useMobileDevice();
   const isStandaloneWindow = useMediaQuery('(display-mode: standalone)');
   const shouldShowAddToHomeOptions = isMobile && !isStandaloneWindow;
-
-  const { refs, floatingStyles, context } = useFloating({
-    open: isOpen,
-    onOpenChange: setIsOpen,
-    placement: 'bottom-end',
-    whileElementsMounted: autoUpdate,
-  });
-
-  const dismiss = useDismiss(context);
-  const hover = useHover(context, { move: false, handleClose: safePolygon() });
-  const focus = useFocus(context);
-  const { getReferenceProps, getFloatingProps } = useInteractions([
-    hover,
-    focus,
-    dismiss,
-  ]);
 
   const handleOpenAboutModal = () => {
     setIsAboutModalOpen(true);
@@ -61,38 +38,46 @@ const MoreButton: React.FC = function () {
 
   return (
     <>
-      <button
-        ref={refs.setReference}
-        className={classnames(
-          'py-2.5 px-3 rounded-lg bg-transparent border-none',
-          'text-white cursor-pointer flex items-center hover:bg-black/20'
-        )}
-        {...getReferenceProps()}
-      >
-        <Icon name="pending" />
-      </button>
-      {isOpen && (
-        <PopperWrapper
-          // eslint-disable-next-line react-hooks/refs
-          ref={refs.setFloating}
-          style={floatingStyles}
-          {...getFloatingProps()}
-        >
-          <PopperHeader>More</PopperHeader>
-          <MenuItem
-            icon={'info'}
-            label="About Classmaid"
-            onClick={handleOpenAboutModal}
-          />
-          {shouldShowAddToHomeOptions && (
-            <MenuItem
-              icon={'add_to_home_screen'}
-              label="Add to Home Screen"
-              onClick={handleOpenAddToHomeScreen}
-            />
-          )}
-        </PopperWrapper>
-      )}
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <button
+            className={classnames(
+              'py-2.5 px-3 rounded-lg bg-transparent border-none',
+              'text-white cursor-pointer flex items-center hover:bg-black/20'
+            )}
+          >
+            <Icon name="pending" />
+          </button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            align="end"
+            sideOffset={0}
+            className={classnames(
+              'min-w-60 max-w-80 rounded-2xl p-2 z-10',
+              'bg-white dark:bg-gray-900 shadow-2xl'
+            )}
+          >
+            <DropdownMenu.Arrow className="fill-white dark:fill-gray-900" />
+            <DropdownMenu.Item
+              className={ItemClassName}
+              onSelect={handleOpenAboutModal}
+            >
+              <Icon name="info" />
+              About Classmaid
+            </DropdownMenu.Item>
+            {shouldShowAddToHomeOptions && (
+              <DropdownMenu.Item
+                className={ItemClassName}
+                onSelect={handleOpenAddToHomeScreen}
+              >
+                <Icon name="add_to_home_screen" />
+                Add to Home Screen
+              </DropdownMenu.Item>
+            )}
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
 
       {isAboutModalOpen && (
         <AboutModal isOpen={isAboutModalOpen} onClose={handleCloseAboutModal} />
